39fe8c5f55e27ead008a5fed90261bcf945a9045
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-06-20 18:45:11 -0300

    feat(table): create module and controller

---
+++ src/table/table.module.ts
@@ -0,0 +1,8 @@
+import { Get, Module } from '@nestjs/common';
+import { TableController } from './table.controller';
+
+@Module({
+  controllers: [TableController],
+  providers: [],
+})
+export class TableModule {}
