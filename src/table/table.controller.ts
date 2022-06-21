fef3a3a979c6e03d13e4e91e5c05aa05ef4000bf
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-06-20 23:49:14 -0300

    in progress

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get, Post } from '@nestjs/common';
+import { TableService } from './table.service';
 
 @Controller('table')
 export class TableController {
+  constructor(private tableService: TableService) {}
+
   @Get()
   findAll() {
-    return 'Buscar todas as mesas';
+    return this.tableService.findAll();
   }
 
   @Post()
---
+++ src/table/table.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TableService {
+  findAll() {
+    return 'Buscar todas as mesas';
+  }
+}
