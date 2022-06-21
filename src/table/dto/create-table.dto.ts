9c4fad5360f7848bfb8c722f63ddb99c34fa0009
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-06-21 16:32:26 -0300

    docs: implement swagger

--- src/table/dto/create-table.dto.ts
+++ src/table/dto/create-table.dto.ts
@@ -1,3 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsPositive } from 'class-validator';
+
 export class CreateTableDto {
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty({
+    description: 'Represents the table number',
+    example: 1,
+  })
   number: number;
 }
