ba00535d7468c5cc05b0947f6bc2b14b5809fd4c
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-06-21 14:45:11 -0300

    feat(table): create dto 'CreateTableDto'

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { stringify } from 'querystring';
+import { CreateTableDto } from './dto/create-table.dto';
 
 @Injectable()
 export class TableService {
   findAll() {
     return 'Buscar todas as mesas';
+  }
+
+  create(createTableDto: CreateTableDto) {
+    return 'Criar nova mesa' + JSON.stringify(createTableDto);
   }
 }
