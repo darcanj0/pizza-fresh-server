ed260a087524e06afb4d0106931c68151d696f86
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-06-22 12:03:03 -0300

    feat(table): find all and create with prisma

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateTableDto } from './dto/create-table.dto';
 import { Table } from './entities/table.entity';
 
 @Injectable()
 export class TableService {
-  tables: Table[] = [];
+  
+  constructor(private readonly prisma: PrismaService) {}
 
-  findAll() {
-    return this.tables;
+  async findAll() {
+    return this.prisma.table.findMany();
   }
 
-  create(createTableDto: CreateTableDto) {
-    const table: Table = { id: 'random_id', ...createTableDto };
-    this.tables.push(table);
-    return table;
+  create(dto: CreateTableDto) {
+    const data: Table = { ...dto };
+    return this.prisma.table.create({ data });
   }
 }
