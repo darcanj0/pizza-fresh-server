416a952fa0378170b4597c4a5a8ec7defaf4d27d
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-06-23 21:44:44 -0300

    chore(table): treat error when creating table

--- src/table/dto/update-table.dto.ts
+++ src/table/dto/update-table.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateTableDto } from './create-table.dto';
 
-export class UpdateTableDto extends PartialType(CreateTableDto) {}
+export class UpdateTableDto extends PartialType(CreateTableDto) {
+    @ApiProperty({
+        description:'Represents the table number',
+        example: 3,
+    })
+    number: number;
+}
