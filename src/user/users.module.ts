ed92a8a2542d6fd784534116d30702e09105e579
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-07-05 01:06:35 -0300

    fix(user): import in user module

--- src/user/users.module.ts
+++ src/user/users.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [PrismaClient],
+  imports: [PrismaModule],
   controllers: [UserController],
   providers: [UserService],
 })
