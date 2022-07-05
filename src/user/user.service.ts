21e1c7e5c554de537ae7965541e5afa7e3a4f2c3
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-07-05 17:29:59 -0300

    feat(users): complete crud in users

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,19 +1,37 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserService {
+  private userSelect = {
+    id: true,
+    email: true,
+    user_name: true,
+    password: false,
+    image: true,
+    created_at: true,
+    updated_at: true,
+  };
   constructor(private readonly prisma: PrismaService) {}
 
   findAll(): Promise<User[]> {
-    return this.prisma.user.findMany();
+    return this.prisma.user.findMany({ select: this.userSelect });
   }
 
   async verifyIdAndReturnUser(id: string): Promise<User> {
-    const record = await this.prisma.user.findUnique({ where: { id } });
+    const record = await this.prisma.user.findUnique({
+      where: { id },
+      select: this.userSelect,
+    });
     if (!record) {
       throw new NotFoundException(`Id ${id} register was not found.`);
     }
@@ -24,19 +42,52 @@ export class UserService {
     return this.verifyIdAndReturnUser(id);
   }
 
-  create(dto: CreateUserDto): Promise<User> {
-    const data = { ...dto };
-    return this.prisma.user.create({ data });
+  async create(dto: CreateUserDto): Promise<User> {
+    if (dto.password != dto.confirm_password) {
+      throw new BadRequestException('Passwords sent are not equal.');
+    }
+    delete dto.confirm_password;
+    const data: User = { ...dto, password: await bcrypt.hash(dto.password, 8) };
+    return this.prisma.user
+      .create({ data, select: this.userSelect })
+      .catch(this.handleUniqueConstraintError);
   }
 
-  async update(id: string, dto: UpdateUserDto) {
+  async update(id: string, dto: UpdateUserDto): Promise<User> {
     await this.verifyIdAndReturnUser(id);
-    const data = { ...dto };
-    return this.prisma.user.update({ where: { id }, data });
+
+    if (dto.password) {
+      if (dto.password != dto.confirm_password) {
+        throw new BadRequestException('Passwords sent are not equal.');
+      }
+    }
+
+    delete dto.confirm_password;
+
+    if (dto.password) {
+      dto = { ...dto, password: await bcrypt.hash(dto.password, 8) };
+    }
+
+    const data: Partial<User> = { ...dto };
+    return this.prisma.user
+      .update({
+        where: { id },
+        data,
+        select: this.userSelect,
+      })
+      .catch(this.handleUniqueConstraintError);
   }
 
   async remove(id: string) {
     await this.verifyIdAndReturnUser(id);
     await this.prisma.user.delete({ where: { id } });
+  }
+
+  handleUniqueConstraintError(error: Error): undefined {
+    const errorLines = error.message?.split('\n');
+    const lastErrorLine = errorLines[errorLines.length - 1]?.trim();
+    throw new UnprocessableEntityException(
+      lastErrorLine || 'An error occurred while executing the operation',
+    );
   }
 }
