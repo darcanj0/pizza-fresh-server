45e1e58ec64e56c403da839791ee9f8a9082e1bb
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-07-10 01:13:15 -0300

    fix(table): error in entity order[]

--- src/table/entities/table.entity.ts
+++ src/table/entities/table.entity.ts
@@ -3,7 +3,7 @@ import { Order } from "src/order/entities/order.entity";
 export class Table {
   id?: string;
   number: number;
-  orders?: Partial<Order> 
+  orders?: Partial<Order>[] 
   created_at?: Date;
   updated_at?: Date;
 }
